b74031d9e67d7204885e8ca353979d9ad0320789
georgeneligan <108015805+georgeneligan@users.noreply.github.com>
2023-03-20 14:30:33 +0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,50 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+if (ScrollTrigger.isTouch !== 1) {
+  ScrollSmoother.create({
+    wrapper: ".wrapper",
+    content: ".content",
+    smooth: 1.5,
+    effects: true,
+  });
+
+  gsap.fromTo(
+    ".hero-section",
+    { opacity: 1 },
+    {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".hero-section",
+        start: "center",
+        end: "820",
+        scrub: true,
+      },
+    }
+  );
+  gsap.fromTo(
+    ".skills_wrapper",
+    { opacity: 1 },
+    {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".skills_wrapper",
+        start: "center",
+        end: "550",
+        scrub: true,
+      },
+    }
+  );
+  gsap.fromTo(
+    ".bottom1",
+    { opacity: 1 },
+    {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".bottom1",
+        start: "center",
+        end: "250",
+        scrub: true,
+      },
+    }
+  );
+}
